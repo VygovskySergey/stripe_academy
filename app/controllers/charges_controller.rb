15379595da5f9f56ffd9c0f17d9d3c2649b4c606
Sergey Vygovsky <vygovskysergey@gmail.com>
2019-01-23 19:38:19 +0100

    Add order completion

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,6 +1,9 @@
 class ChargesController < ApplicationController
+  before_action :set_order, only: :create
+
   def new
-    @amount = params[:amount].to_f
+    @amount   = params[:amount].to_f
+    @order_id = params[:order_id]
   end
 
   def create
@@ -11,13 +14,23 @@ class ChargesController < ApplicationController
 
     charge = Stripe::Charge.create(
       customer:    customer.id,
-      amount:      params[:amount].to_f,
+      amount:      params[:amount],
       description: 'Rails Stripe customer',
       currency:    'usd'
     )
 
+    order.update(state: 'completed')
+
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
+  end
+
+  private
+
+  attr_reader :order
+
+  def set_order
+    @order = Order.find(params[:order_id])
   end
 end
